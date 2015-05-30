7c0c2c03dfb7ec79e2acd930a7a99e1cfb8127d8
Shlomi Fish <shlomif@shlomifish.org>
2015-05-30 16:23:05 +0300

    Fix resources

--- lib/SDLx/Betweener.pm
+++ lib/SDLx/Betweener.pm
@@ -7,7 +7,7 @@ use Scalar::Util qw(weaken);
 use SDL;
 use SDLx::Betweener::Timeline;
 
-our $VERSION = '0.0104';
+our $VERSION = '0.0105';
 
 require XSLoader;
 XSLoader::load('SDLx::Betweener', $VERSION);
