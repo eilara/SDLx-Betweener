2b684bca3e8f2716638335d61ae38a73faaa8d6a
Shlomi Fish <shlomif@shlomifish.org>
2015-05-30 16:03:16 +0300

    Bump version

--- lib/SDLx/Betweener.pm
+++ lib/SDLx/Betweener.pm
@@ -7,7 +7,7 @@ use Scalar::Util qw(weaken);
 use SDL;
 use SDLx::Betweener::Timeline;
 
-our $VERSION = '0.0103';
+our $VERSION = '0.0104';
 
 require XSLoader;
 XSLoader::load('SDLx::Betweener', $VERSION);
